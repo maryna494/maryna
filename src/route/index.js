b8c92f55f8c65dd3a5746cb8c576826755ef38cd
maryna494 <martynovam019@gmail.com>
2023-09-18 21:05:03 +0300

    end 1 task

--- src/route/index.js
+++ src/route/index.js
@@ -686,6 +686,8 @@ router.get('/js', function (req, res) {
 
   //            ↙ cюди вводимо назву файлу з сontainer
   res.render('js', {
+    loyout: 'basic',
+
     name: 'JavaScript',
     description:
       'JavaScript is a popular high-level, dynamic, and interpreted programming language.',
